f901fe7fc6024830b47e689305a348894597a46e
andypay <andrewpay@gmail.com>
2017-03-18 14:43:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,3 @@
-Skip to content
-This repository
-Search
-Pull requests
-Issues
-Gist
- @andypay
- Sign out
- Watch 3
-  Star 4
-  Fork 38 svet4/shipping-costs-sample
- Code  Issues 0  Pull requests 2  Projects 0  Wiki  Pulse  Graphs
-Branch: master Find file Copy pathshipping-costs-sample/app.py
-1919912  on Aug 6, 2016
- svetlanapastukhova py and json files
-0 contributors
-RawBlameHistory     
-Executable File  59 lines (41 sloc)  1.31 KB
 #!/usr/bin/env python
 
 import urllib
